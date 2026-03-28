10ccd30cb3a2fea723e2efb04718dda31614a943
shuoguo-leilei <shuoguo-leilei@users.noreply.github.com>
2026-03-28 14:40:49 +0000

    deploy: shuoguo-leilei/blog-source@02e510d5b53a134b0eddce0f25780d11b86464f7

--- custom/js/countdown.js
+++ custom/js/countdown.js
@@ -1,6 +1,6 @@
 const CountdownTimer = (() => {
     const config = {
-        targetDate: "2026-02-26",
+        targetDate: "2027-02-06",
         targetName: "春节",
         units: {
             day: { text: "今日", unit: "小时" },
